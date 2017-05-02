51d4888fc665a9c0eefe722552257e3ad29f8a76
Pedro Martín del Campo González <pedro.martindelcampo.gonzalez@gmail.com>
2017-05-02 16:39:01 -0700

    Revision

--- InceptionPluginManagement/src/inception/plugin/PluginManager.java
+++ InceptionPluginManagement/src/inception/plugin/PluginManager.java
@@ -9,8 +9,6 @@ import inception.model.Stimulus;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
